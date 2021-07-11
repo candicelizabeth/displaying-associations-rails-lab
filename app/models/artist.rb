f0104500324d6ffec79df19899838f39bf71087f
Candice Peters <candice.peters@flatironschool.com>
2021-07-11 00:50:38 -0500

    commit tree 1d7921ca4f631c6ad216a59a205ba631acff8c50

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,11 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count_full
+        "#{self.name} #{self.song_count} songs"
+    end
+
+    def song_count 
+        self.songs.count
+    end
 end
